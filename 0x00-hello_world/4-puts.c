fd6fdbacdd987cf4b2eb5d34144dfea358c2a830
Daniel Siman <siman.aken@gmail.com>
2022-12-10 10:19:11 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/**
- * main - Prints ""Programming is like building a multilingual puzzle"
- */
+/**                                                                                                                                     
+ *  * main - Entry                                                                                                                         
+ *  * Return: Always 0                                                                                                                     
+ *  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
