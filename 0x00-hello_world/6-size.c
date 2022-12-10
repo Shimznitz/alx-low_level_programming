b191574be6cab41da6afe9dc46e231b103dabd34
Daniel Siman <siman.aken@gmail.com>
2022-12-10 11:38:35 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long int longInt;
 	long long int longLongInt;
 	float float1;
+
 	printf("Size of a char: %i byte(s)\n", sizeof(char1));
 	printf("Size of an int: %i byte(s)\n", sizeof(int1));
 	printf("Size of a long int: %i byte(s)\n", sizeof(longInt));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(longLongInt));
 	printf("Size of a float: %i byte(s)\n", sizeof(float1));
-
 	return (0);
 }
