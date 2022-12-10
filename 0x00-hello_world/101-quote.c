91abd40d9849b6fbe7d48e1f4a2f26540733cac2
Daniel Siman <siman.aken@gmail.com>
2022-12-10 15:05:51 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  * Return: Always 1
  */
 int main(void)
-{
-fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
