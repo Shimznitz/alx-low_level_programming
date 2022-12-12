3da8a62983e8f2d999199d929db3128691e8db1e
Daniel Siman <siman.aken@gmail.com>
2022-12-12 06:43:21 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,7 @@ int main(void)
 		printf("Last digit of %i is %d and is 0\n", n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
 		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
-
+	else
+		printf("f");
 		return (0);
 }
