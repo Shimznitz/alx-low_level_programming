3debf3c2349578b4ed1d30601ac62650f055d8aa
Daniel Siman <siman.aken@gmail.com>
2022-12-12 12:37:29 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch < '10'; ++ch)
+		putchar'ch';
+
+return (0);
+}
