68784628f987d32a1b26992b5832f03c1b4adddd
Daniel Siman <siman.aken@gmail.com>
2022-12-12 13:00:24 -0800

    smile in mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints reverse alphabets
+ * Return: Always 0
+ */
+int main(void)
+{
+	 int ch = 'z';
+
+	 while (ch >= 'a')
+		 putchar(ch);
+	 	 --ch;
+ putchar('\n');
+
+ return (0);
+}
