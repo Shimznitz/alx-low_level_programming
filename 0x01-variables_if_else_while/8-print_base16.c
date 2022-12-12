03cb772a394f9497cc30151ce4a0225ff2246bb4
Daniel Siman <siman.aken@gmail.com>
2022-12-12 13:30:29 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints base 16 numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = '0';
+	char letter - 'a';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
