9682422cda8ffdbe2afd192076f47a82afd7607b
Daniel Siman <siman.aken@gmail.com>
2022-12-14 14:24:28 -0800

    n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abbs(int) - prints abs value
+ * @int: does stuff
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	int x;
+	x = abs(n);
+	return (n);
+}
