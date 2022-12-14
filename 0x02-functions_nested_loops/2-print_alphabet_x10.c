3fef58fb5236157fad1288d647eab02c6723af1b
Daniel Siman <siman.aken@gmail.com>
2022-12-14 05:23:27 -0800

    corrections

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_alphabet_x10(void) - prints lowercase letters 10 times
+ * Return: Always void
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; c <= 'z'; ch++)
+			putchar(ch);
+	}
+	putchar('\n');
+}
