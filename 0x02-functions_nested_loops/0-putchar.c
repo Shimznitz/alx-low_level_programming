f082b4b1847e4880b8c76ae6a5e4b71d7939ee91
Daniel Siman <siman.aken@gmail.com>
2022-12-14 03:23:09 -0800

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * main - prints _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch[] = ("_putchar") /*_putschar array */
+	int i = 0; /* iterator */
+
+	for (i = 0; i <= 8; i++)
+		putchar(ch[i]);
+
+	return (0);
+}
