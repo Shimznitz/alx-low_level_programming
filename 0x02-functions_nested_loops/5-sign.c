8d9ec0397251b3adfe2c7206a5c66fd5cbd431fd
Daniel Siman <siman.aken@gmail.com>
2022-12-14 09:47:34 -0800

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,8 @@
  */
 int print_sign(int n)
 {
+	char i = '1';
+	char z = '0';
 	char positive_sign = '+';
 	char negative_sign = '-';
 	char zero_sign = '0';
@@ -15,16 +17,19 @@ int print_sign(int n)
 	{
 		return (1);
 		putchar(positive_sign);
+		putchar(i);
 	}
 	else if (n < 0)
 	{
 		return (-1);
 		putchar(negative_sign);
+		putchar(i);
 	}
 	else
 	{
 		return (0);
 		putchar(zero_sign);
+		putchar(z);
 	}
 	putchar('\n');
 }
