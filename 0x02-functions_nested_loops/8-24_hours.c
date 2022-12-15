3f3378ef9cf6e4da236e28cde86b58f73e329865
Daniel Siman <siman.aken@gmail.com>
2022-12-15 04:10:34 -0800

    m

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - prints time sequenntially
+ * Return: Always void
+ */
+void jack_bauer(void)
+{
+	char a = 48;
+	char b = 48;
+	char c = 48;
+	char d = 48;
+
+	for (a = 48; a <= 49; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(':');
+					putchar(c);
+					putchar(d);
+					putchar('\n');
+				}
+			}
+		}
+
+	}
+	char a = 48;
+
+	for (b = 48; b <= 51; b++)
+	{
+		for (c = 48; c <= 53; c++)
+		{
+			for (d = 48; d <= 57; d++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(':');
+				putchar(c);
+				putchar(d);
+				putchar('\n');
+			}
+		}
+	}
+}
