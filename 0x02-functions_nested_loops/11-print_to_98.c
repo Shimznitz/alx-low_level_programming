08e5011c078c1090a97715088fdf6d9545480985
Daniel Siman <siman.aken@gmail.com>
2022-12-15 13:40:46 -0800

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,11 +10,11 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n >= 98, n--)
+		for (n >= 98; n--)
 		{
 			printf("%d, ", n);
 		}
-		printf('\n');
+		printf("\n");
 	}
 	else if (n <= 98)
 	{
