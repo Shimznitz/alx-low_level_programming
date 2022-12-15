b3a9b60421319c8a9d33c9b1cbd3b1427491d3a8
Daniel Siman <siman.aken@gmail.com>
2022-12-15 13:10:17 -0800

    m

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_to_98 - prints from 1 to 98
+ * @n: input number
+ * Return: Always int
+ */
+void print_to_98(int n)
+{
+	for (n; n <= 98; n++)
+	{
+		printf("%d, ",n);
+	}
+}
