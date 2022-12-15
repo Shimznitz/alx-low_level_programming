2c7870e1e8420e5694ba69d2c328b0fe0734444f
Daniel Siman <siman.aken@gmail.com>
2022-12-14 16:12:07 -0800

    m

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <>
+#include <>
+/**
+ * print_last_digit - prints len of nums
+ * @n: manipulated
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	int b = floor(logb(abs(n))) + 1;
+	return (b);
+}
