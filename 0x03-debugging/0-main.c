a37d7eb44f1e0ff635ad9f26aa584297e597cd6c
Daniel Siman <siman.aken@gmail.com>
2022-12-15 16:47:20 -0800

    m

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mainf - tests function that prints if integer is positive or negative
+ * main - tests func that prints if integer is positive or negative
  * Return: 0
  */
 
