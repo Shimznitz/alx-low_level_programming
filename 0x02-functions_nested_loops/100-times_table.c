2aa64471413902d1dd496e8d075131c8ecc10a6b
Daniel Siman <siman.aken@gmail.com>
2022-12-17 13:51:52 -0800

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar((prd /10) % 10 + '0');
+				_putchar((prd / 10) % 10 + '0');
 				_putchar(prd % 10 + '0');
 			}
 			else if (prd > 99 && prd < 1000)
