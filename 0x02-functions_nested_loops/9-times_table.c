0e75222f5eca7b1bc44e1dc41221186c444a7804
Daniel Siman <siman.aken@gmail.com>
2022-12-17 12:11:56 -0800

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,35 @@
  */
 void times_table(void)
 {
-	int  b;
-	char ch = '0';
+	int row, column, product, tens, ones;
 
-	for (b = 0; b <= 9; b++)
+	for (row = 0); row <= 9; row++
 	{
-	if (b > 1)
+	for (column = 0; column <=9; column++)
 	{
-		b *= 9;
-		printf("%d, ", b);
+		product = row * column;
+		tens = product / 10;
+		ones = product % 10;
+
+		if (column == o)
+		{
+			_putchar('0');
+		}
+		else if (product < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(ones + '0');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(tens + '0');
+			_putchar(ones + '0');
+		}
 	}
-	else
-	{
-		putchar(ch);
-		putchar(',');
-		putchar(' ');
-		for (i = b; )
-	}
-	printf("\n");
+	_putchar('\n');
 	}
 }
