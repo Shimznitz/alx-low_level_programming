8d65875667235e334030fda00dd23b522e309136
Daniel Siman <siman.aken@gmail.com>
2022-12-18 06:19:34 -0800

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - Checks for upper case
+ * @c: input character
+ * Return: 1 if uppercase, 0 if lowercase
+ */
+int _isupper(int c)
+{
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper - 1;
+			break;
+		}
+	}
+
+	return (isupper);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*declarations go here*/
+int _isupper(int c);
+#endif
+
