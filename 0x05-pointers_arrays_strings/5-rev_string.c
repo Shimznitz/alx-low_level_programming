c937d25dd17a09dd297168cc135ddfecd90fa559
Daniel Siman <siman.aken@gmail.com>
2022-12-19 15:52:10 -0800

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+/**
+ * rev_string - reverse string
+ * @c: input char
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int count = strlen(s);
+	int i;
+
+	for (i = count; i >= count; --i)
+	{
+		if (s[i] != '\n')
+		{
+			printf("%c", s[i]);
+		}
+	}
+}
