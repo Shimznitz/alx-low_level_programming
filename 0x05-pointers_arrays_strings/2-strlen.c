9bbfd881dea3d563bb65376ebc3a7bb0e24d2b49
Daniel Siman <siman.aken@gmail.com>
2022-12-19 09:36:46 -0800

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - len of string
+ * @s: string in
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	int str_len;
+
+	str_len = strlen(s);
+
+	return (str_len);
+}
