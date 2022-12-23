1aef88d9fc68ea7e0c009a6500946cf406ccd0a0
Daniel Siman <siman.aken@gmail.com>
2022-12-23 09:17:43 -0800

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int *i = strcmp(s1, s2);
+	int i = 0;
 
-	return (i);
+	if ((s1[i] != '\0' || s2[i] != '\0') && (s1[i] == s2[i]))
+	{
+		++i;
 
+		return (0);
+	}
+	else
+		return (1);
 }
