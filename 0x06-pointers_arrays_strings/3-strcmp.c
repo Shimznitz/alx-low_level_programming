075d6c41a41490328ecab9cc2f9e23c6c71c25f9
Daniel Siman <siman.aken@gmail.com>
2022-12-23 08:42:19 -0800

    m

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compare string
+ * @s1: para 1
+ * @s2: para 2
+ * Return: num
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = strcmp(s1, s2);
+
+	return (i);
+
+}
