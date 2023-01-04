27d0d8d41c250be12a18b669c8866300d968666e
Daniel Siman <siman.aken@gmail.com>
2023-01-04 10:01:00 -0800

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "main.h"
 /**
  * _puts_recursion - prints char
  * @s: character
@@ -5,9 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s != '\0')
-		_putchar(s);
-		_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(*s);
+	}
 	else
 		_putchar('\n');
 }
