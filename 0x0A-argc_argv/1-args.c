49fbb18061be214323fc708d9cdb9ce913ebe42f
Daniel Siman <siman.aken@gmail.com>
2023-01-11 16:51:33 -0800

    2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints number of arg
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return(0);
+}
