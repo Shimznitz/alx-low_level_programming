fce6f049e9e22ba433927d8bd381b698278de123
Daniel Siman <siman.aken@gmail.com>
2023-01-11 16:56:01 -0800

    m

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * @argv: arg vector
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 
-	return(0);
+	return (0);
 }
