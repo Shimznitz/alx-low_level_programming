11f50daeda568c3f6467c6acc7dfc25a5f168c06
Daniel Siman <siman.aken@gmail.com>
2023-01-11 16:45:55 -0800

    adjusted

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
  * main - prints its name
  * @argc: number of arguments
@@ -7,7 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
+	if (argc > 0)
+	{
 	printf("%s\n", argv[0]);
+	}
 
 	return (0);
 }
