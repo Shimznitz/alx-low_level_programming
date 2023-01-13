5b990e06da68e50994f3fdc34f33d52391201518
Daniel Siman <siman.aken@gmail.com>
2023-01-13 11:52:00 -0800

    copies one string another

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a poinere to a string
+ * @str: pointer
+ * Return: pointer to a string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	ptr = malloc(strlen(str) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < strlen(str) + 1; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
