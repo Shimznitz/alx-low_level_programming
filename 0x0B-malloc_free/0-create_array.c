7d3aea6f86bb75a041438abca07fbe1b3e5bd797
Daniel Siman <siman.aken@gmail.com>
2023-01-13 10:29:34 -0800

    using malloc to allocate memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.hi>
+/**
+ * *create_array - prints buffer in hexa
+ * @size: size of array
+ * @c: character
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr; /*declaring the pointer to the character array created*/
+	ptr = malloc(size * sizeof(char)); /*memory allocated using malloc*/
+
+	return (ptr);
+}
