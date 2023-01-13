60cd9aa15263670658973ddc1c633fd8d5118f31
Daniel Siman <siman.aken@gmail.com>
2023-01-13 10:51:19 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	c = malloc(size * sizeof(char)); /*memory allocated using malloc*/
-	c[0] = 'a';
-	return (c);
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * sizeof(char)); /*memory allocated using malloc*/
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
 }
