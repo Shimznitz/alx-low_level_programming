f3314625302cea712e106a74db6e14ea779a2a89
Daniel Siman <siman.aken@gmail.com>
2023-01-17 15:28:38 -0800

    using struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - initializes a variable with struct dog
+ * @d: array
+ * @name: pointer to arrray
+ * @age: float
+ * @owner: char to array
+ * Description: Struct for DOg
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
