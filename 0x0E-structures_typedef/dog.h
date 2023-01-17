a0dbb593bbdd55c567eb238a9d6b477d1c1cb04c
Daniel Siman <siman.aken@gmail.com>
2023-01-17 15:08:06 -0800

    using struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains elements of a dogs identity
+ * @name: member string name
+ * @age: int of age
+ * @owner: memeber with char owner
+ * Description - a normal description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
