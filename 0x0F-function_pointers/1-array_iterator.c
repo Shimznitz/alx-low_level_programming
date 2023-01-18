9503c06669802fd3f93d73bf532c722aefbd5a04
Daniel Siman <siman.aken@gmail.com>
2023-01-18 07:27:51 -0800

    adjusted the documentation

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,12 +6,13 @@
  * @array: an arrray of elements
  * @size: size of array
  * @action: funtion
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	for (; i < 5; i++)
+	for (; i < size; i++)
 	{
 	action(array[i]);
 	}
