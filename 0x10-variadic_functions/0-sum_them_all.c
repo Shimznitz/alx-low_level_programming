d729c44dd3a2211579ea48beec7e9f2851dd1d8d
Daniel Siman <siman.aken@gmail.com>
2023-01-21 14:12:18 -0800

    fixed the betty doc

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list var;
 
 	if (n == 0)
+	{
 		return (0);
+	}
 
 	va_start(var, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(var, int);
 	}
