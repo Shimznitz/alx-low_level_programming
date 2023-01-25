e41671216120a2dd8bad1397eb723e67ecbf37c7
Daniel Siman <siman.aken@gmail.com>
2023-01-25 15:31:06 -0800

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,15 @@ void rev_string(char *s)
 	char first, last;
 
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	max = i - 1;
 	half = max / 2;
+
 	while (half >= 0)
 	{
 		first = s[max - half];
