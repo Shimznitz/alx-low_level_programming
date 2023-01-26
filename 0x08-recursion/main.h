47fad60611dbdd12605323da94348be94d8088c7
Daniel Siman <siman.aken@gmail.com>
2023-01-25 16:18:12 -0800

    correction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,7 @@ int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
 #endif /* MAIN_H */
