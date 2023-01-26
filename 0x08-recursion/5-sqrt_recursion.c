36407bb469f651b777a1b00a3044c1a0c8cb63bf
Daniel Siman <siman.aken@gmail.com>
2023-01-25 16:20:58 -0800

    correctiom

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,28 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * _sqrt_recursion - finds the squarerooot
- * @n: number to be rooted
- * Return: returns an integer
- */
+   * _sqrt_recursion - main funct
+    * @n: int n
+     * Return: int
+      */
 int _sqrt_recursion(int n)
 {
+	return (_sqrt(n, 1));
+}
+
+/**
+   * _sqrt - _sqrt_recursion
+    * @n: integer paramtr
+     * @i: integer parameter
+      * Return: sqrt
+       */
+int _sqrt(int n, int i)
+{
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else
-	{
-	return ((_sqrt_recursion(n) + (n / _sqrt_recursion(n))) / 2);
-	}
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
