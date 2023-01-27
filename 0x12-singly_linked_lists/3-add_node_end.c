61a40aff03d20a56fda00082fed02304836ca963
Daniel Siman <siman.aken@gmail.com>
2023-01-26 19:45:43 -0800

    m

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
-   * add_node_end - adds a new node at the end
-    * @head: head of the linked list.
-      * @str: string to store in the list.
-       * Return: address of the head.
-        */
+ * add_node_end - adds a new node at the end
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
@@ -30,8 +30,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		while (temp->next != NULL)
-			temp = temp->next;
+		{
+		temp = temp->next;
 		temp->next = new;
+		}
 	}
 
 	return (*head);
