22915ac06c5b822954024b72ecc197d49c5336f9
Daniel Siman <siman.aken@gmail.com>
2023-01-31 08:34:59 -0800

    printing elements from a struct

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+/**
+ * print_listint - prints all elements of a list
+ * @h: pointer to struct listint
+ * Return: a sizeof
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *prt;
+	prt = malloc(sizeof(listint_t));
+
+	prt = h;
+
+	if (prt->h != NULL)
+	{
+		return (prt->n);
+	}
+}
