f0985e670982d524afe308bafdf3777647c70511
Daniel Siman <siman.aken@gmail.com>
2023-01-31 09:43:34 -0800

    printing just elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a list
+ * @h: pointer to struct listint
+ * Return: a sizeof
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
