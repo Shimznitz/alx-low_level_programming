b931bccda85331bc63398f565f73af0030ddf22b
Daniel Siman <siman.aken@gmail.com>
2023-01-31 09:47:52 -0800

    corrected the prototype spelling in the function file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * print_listint - prints all elements of a list
+ * listint_len - prints all elements of a list
  * @h: pointer to struct listint
  * Return: a sizeof
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
