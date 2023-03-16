e248598527e7726c30537a17cb64eddabd8245fa
Daniel Siman <siman.aken@gmail.com>
2023-03-15 17:09:51 -0700

    correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, addition = 0;
+    int sum = 0;
+    int i, j;
 
-	for (i = 1; i < argc; i++)
+    if (argc == 1)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++)
+    {
+        for (j = 0; argv[i][j] != '\0'; j++)
 	{
-		for (; = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		addition += atoi(argv[i]);
-	}
-	printf("%d\n", add);
-	return (0);
-}
+            if (!isdigit(argv[i][j]))
+	{
+                printf("Error\n");
+                return 1;
+            }
+        }
+        sum += atoi(argv[i]);
+    }
 
+    printf("%d\n", sum);
+    return 0;
+}
