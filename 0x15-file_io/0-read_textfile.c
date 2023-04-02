2e6f4afa72c95be3e97eff23bd54009a689a7648
Daniel Siman <siman.aken@gmail.com>
2023-04-01 18:36:18 -0700

    jj

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: file created
+ * @letters: number of characters
+ * Return: a signed or unsigned integer
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
