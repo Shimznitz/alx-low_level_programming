96f709910c5c7d98698b470d060a7b7f15eb3749
root <Shimznitz>
2023-06-14 16:48:07 -0700

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
- * main - adds multiple numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
+*main - prints the minimum number of coins to make change
+* for an amount of money
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 1 if there is an error; else returns 0
+*/
+
 int main(int argc, char *argv[])
 {
-    /* Check if the number of arguments passed is exactly 2 */
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
-    /* Convert the input parameter to an integer using atoi */
-    int cents = atoi(argv[1]);
-    /* Check if the input parameter is negative */
-    if (cents < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
-    /* Calculate the number of coins required */
-    int num_quarters = cents / 25;
-    cents -= num_quarters * 25;
-    int num_dimes = cents / 10;
-    cents -= num_dimes * 10;
-    int num_nickels = cents / 5;
-    cents -= num_nickels * 5;
-    int num_twos = cents / 2;
-    cents -= num_twos * 2;
-    int num_ones = cents;
-    /* Print the number of coins required */
-  printf("%d\n", num_quarters + num_dimes + num_nickels + num_twos + num_ones);
-    return 0;
+int cents, coins = 0;
+
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
 }
+cents = atoi(argv[1]);
+while (cents > 0)
+{
+	coins++;
+	if ((cents - 25) >= 0)
+	{
+		cents -= 25;
+		continue;
+	}
+	if ((cents - 10) >= 0)
+	{
+		cents -= 10;
+		continue;
+	}
+	if ((cents - 5) >= 0)
+	{
+		cents -= 5;
+		continue;
+	}
+	if ((cents - 2) >= 0)
+	{
+		cents -= 2;
+		continue;
+	}
+	cents--;
+}
+printf("%d\n", coins);
+return (0);
+}
+
