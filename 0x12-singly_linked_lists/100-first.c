2dbbaf23f77a83c33d978cfacfe1578bd1f6aa1b
root <Shimznitz>
2023-07-06 16:44:38 -0700

    m

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,12 @@
 #include "main.h"
-void first(void)
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - Prints a string before executing the main function
+ */
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,
 			\nI bore my house upon my back!\n");
-
 }
